304707166c6b5bbd58a74a94b6fbe90b065b0e4c
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-22 15:41:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,22 @@
 # Your code goes here!
 class Anagram
 
-  attr_accessor :match
-  def initialize(match = [])
-    @match = match
+  attr_accessor :word, :final
+
+  def initialize(word)
+    @word = word
+    @final = []
+  end
+
+  def match(list)
+    wordArr = @word.split("").sort
+
+    list.each do |x|
+      if x.split("").sort == wordArr
+        @final << x
+      end
+    end
+    @final
   end
 
 end
