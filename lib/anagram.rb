1d7bc2c3ab636c5733c1d85eab566ace443671e3
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-22 15:42:04 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,16 +7,16 @@ class Anagram
     @word = word
     @final = []
   end
-
-  def match(list)
-    wordArr = @word.split("").sort
-
-    list.each do |x|
-      if x.split("").sort == wordArr
-        @final << x
-      end
-    end
-    @final
-  end
+  # 
+  # def match(list)
+  #   wordArr = @word.split("").sort
+  #
+  #   list.each do |x|
+  #     if x.split("").sort == wordArr
+  #       @final << x
+  #     end
+  #   end
+  #   @final
+  # end
 
 end
