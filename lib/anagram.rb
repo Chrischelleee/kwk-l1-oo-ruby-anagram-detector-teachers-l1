7234693d64aa7b2e5e26586497aaeab93dae18d6
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-22 15:47:46 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,20 +3,20 @@ class Anagram
 
   attr_accessor :word, :final
 
-  def initialize(word)  #takes in the word for the anagram to detect 
-    @word = word        # what were trying to detect 
-    @final = []         #final array where words will be store if anagram ever detects a word 
+  def initialize(word)  #takes in the word for the anagram to detect
+    @word = word        # what were trying to detect
+    @final = []         #final array where words will be store if anagram ever detects a word
   end
-  
+
   def match(list)
-    wordArr = @word.split("").sort
-  
-    list.each do |x|
-      if x.split("").sort == wordArr
-        @final << x
+    wordArr = @word.split("").sort #splitting the words via strings
+
+    list.each do |x|        #going through each of the words
+      if x.split("").sort == wordArr        #looks for words to put in the final array
+        @final << x           #pushing the words inside the newest array
       end
     end
-    @final
+    @final      #calling out the array
   end
 
 end
