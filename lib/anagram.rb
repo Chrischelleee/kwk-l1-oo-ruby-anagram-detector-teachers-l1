ca69de943463f9d4419ef944ba4ebbed74515353
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-22 15:49:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,22 +1,22 @@
 # Your code goes here!
 class Anagram
 
-  attr_accessor :word, :final
+  attr_accessor :word, :last
 
   def initialize(word)  #takes in the word for the anagram to detect
     @word = word        # what were trying to detect
-    @final = []         #final array where words will be store if anagram ever detects a word
+    @last = []         #final array where words will be store if anagram ever detects a word
   end
 
   def match(list)
-    wordArr = @word.split("").sort #splitting the words via strings
+    words = @word.split("").sort #splitting the words via strings
 
-    list.each do |x|        #going through each of the words
-      if x.split("").sort == wordArr        #looks for words to put in the final array
-        @final << x           #pushing the words inside the newest array
+    list.each do |a|        #going through each of the words
+      if a.split("").sort == words      #looks for words to put in the final array
+        @last << a         #pushing the words inside the newest array
       end
     end
-    @final      #calling out the array
+    @last      #calling out the array
   end
 
 end
